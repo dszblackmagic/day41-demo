6da262f542fd01b100d6e8d3004f506924cbf34f
DDDeng <791284737@qq.com>
2020-04-13 11:04:45 +0800

    idea update

--- src/main/java/cn/git/demo/DemoTest.java
+++ src/main/java/cn/git/demo/DemoTest.java
@@ -3,5 +3,6 @@ package cn.git.demo;
 public class DemoTest {
     public static void main(String[] args) {
         System.out.println("这是我们的git测试");
+        System.out.println("这是我们idea的修改");
     }
 }
